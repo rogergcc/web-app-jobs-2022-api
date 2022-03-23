f31ea3a801f24cc2250ba524cd37fe077b33714f
Roger Colquehuanca <rogcolquehuancac@gmail.com>
2022-03-23 12:50:14 -0500

    waitForNavigation and change delay 3s

--- app/app.js
+++ app/app.js
@@ -132,18 +132,19 @@ const getLinkedinJobs = async (jobsSearch) => {
     // const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
+    const navigationPromise = page.waitForNavigation({waitUntil: "domcontentloaded"});
     // page.on('console', consoleObj => console.log(consoleObj.text()))
 
     const SEARCH_URL = `https://www.linkedin.com/jobs/search?keywords=${jobsSearch}&location=Per%C3%BA&geoId=102927786&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0`;
 
     await page.goto(SEARCH_URL);
-
-    await delay(5000);
+    await navigationPromise;
+    await delay(3000);
 
     await page.waitForSelector(".jobs-search__results-list");
-    const datos = await page.waitForSelector(
-      "section.two-pane-serp-page__results-list > ul > li:nth-child(1) > div > a"
-    );
+    // const datos = await page.waitForSelector(
+    //   "section.two-pane-serp-page__results-list > ul > li:nth-child(1) > div > a"
+    // );
 
     const getLinkedinJobs = await page.evaluate(() => {
       const jobsList = [];
